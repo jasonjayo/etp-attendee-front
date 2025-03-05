95367820227d12ddd5c6d4a7ee2d99b51b585082
jasonjayo <jasonjayo2002@gmail.com>
2025-03-05 17:14:57 +0000

    path change

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const isGithubPages = process.env.GITHUB_PAGES === "true";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isGithubPages ? `/${"/etp-attendee-front"}` : "",
-  assetPrefix: isGithubPages ? `/${"/etp-attendee-front"}/` : "",
+  basePath: isGithubPages ? `/etp-attendee-front` : "",
+  assetPrefix: isGithubPages ? `/etp-attendee-front/` : "",
   trailingSlash: true,
   images: {
     unoptimized: true,
