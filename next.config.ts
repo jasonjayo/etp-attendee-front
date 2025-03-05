58ebba0e42572649a35bad5bd4e06f786911d9d9
jasonjayo <jasonjayo2002@gmail.com>
2025-03-05 14:51:00 +0000

    updated base path

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: false,
   },
+  output: "export",
+  trailingSlash: true, // Ensures proper file-based routing
+  images: {
+    unoptimized: true, // Needed for Next.js Image component in static mode
+  },
+  basePath: "/etp-attendee-front",
 };
 
 export default nextConfig;
