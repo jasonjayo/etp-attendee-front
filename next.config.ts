3a0a0642d4c2d092aa00f4f582253280e41b308e
jasonjayo <jasonjayo2002@gmail.com>
2025-03-05 17:11:29 +0000

    updated config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/etp-attendee-front",
+  basePath: isGithubPages ? `/${"/etp-attendee-front"}` : "",
+  assetPrefix: isGithubPages ? `/${"/etp-attendee-front"}/` : "",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
