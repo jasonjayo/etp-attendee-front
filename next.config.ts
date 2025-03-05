76385c3788cf556e5a4164c83561868cb878019c
jasonjayo <jasonjayo2002@gmail.com>
2025-03-05 15:31:23 +0000

    updated config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   basePath: "/etp-attendee-front",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
