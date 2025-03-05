2dfc3af363243b50ca2063a363a7edd66d275f98
jasonjayo <jasonjayo2002@gmail.com>
2025-03-05 17:32:09 +0000

    config update

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,6 +2,7 @@
 
 import { Navbar } from "flowbite-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
@@ -21,13 +22,12 @@ export default function Header() {
       {/* Navbar Menu */}
       <Navbar.Collapse className="font-medium flex flex-col md:flex-row md:space-x-8 rtl:space-x-reverse mt-4 md:mt-0 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <div className="flex flex-col md:flex-row md:space-x-8 text-md py-3 px-4 md:py-0">
-          <Navbar.Link
+          <Link
             href="/browse"
-            active
             className="py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 dark:text-white md:dark:text-blue-500"
           >
             Home
-          </Navbar.Link>
+          </Link>
         </div>
       </Navbar.Collapse>
     </Navbar>
