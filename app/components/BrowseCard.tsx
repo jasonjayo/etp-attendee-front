31ebfbf4d68991c56d2c3803a1673cce7598a45c
jasonjayo <jasonjayo2002@gmail.com>
2025-03-15 05:21:42 +0000

    update paths

--- app/components/BrowseCard.tsx
+++ app/components/BrowseCard.tsx
@@ -4,7 +4,6 @@ import { Event } from "../services/eventService";
 import { Card } from "flowbite-react";
 import { Button } from "flowbite-react";
 import Link from "next/link";
-import { imagesBasePath } from "../imagesBase";
 
 interface BrowseCardProps {
   event: Event;
@@ -14,7 +13,7 @@ export default function BrowseCard({ event }: BrowseCardProps) {
   return (
     <Card
       className="max-w-sm"
-      imgSrc={`${imagesBasePath}images/${event.image}`}
+      imgSrc={`${process.env.BASE_PATH}/images/${event.image}`}
     >
       <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         {event.title}
